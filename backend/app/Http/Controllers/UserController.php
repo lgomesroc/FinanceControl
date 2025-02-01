40dd3abf44442a2d388db7004767a995b801456e
Douglas Fernandes <98003907+douglasfdev@users.noreply.github.com>
2025-02-01 18:27:34 -0300

    fix(user): add factory and seed the database

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -11,7 +11,7 @@ class UserController extends Controller
     // Lista todos os usuários
     public function index(Request $request)
     {
-        $users = User::select('name', 'email')->get();
+        $users = User::select('name', 'email', 'id')->get();
 
         // API: retorna JSON
         if ($request->is('api/users')) {
